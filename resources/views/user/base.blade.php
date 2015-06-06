4c62ea9617d3ee668779e1ed39b7f697594e2a57
Kurien Zacharia <kurienzach@gmail.com>
2015-06-07 00:18:16 +0530

    Initial Commit

---
+++ resources/views/user/base.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="">
+        <title>annam homely food at your door step</title>
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- icon fonts -->
+        <link href="css/et-icons.css" rel="stylesheet">
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <link href="css/animsition.css" rel="stylesheet">
+        <link href="css/animate.css" rel="stylesheet">
+
+        <!-- Owl Carousel Assets -->
+        <link href="owl-carousel/owl.carousel.css" rel="stylesheet">
+        <link href="owl-carousel/owl.theme.css" rel="stylesheet">
+        <link href="cubeportfolio/css/cubeportfolio.css" rel="stylesheet">
+        <link href="css/lightbox.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="css/style.css" rel="stylesheet">
+
+        <!-- Custom Fonts -->
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,800,700,600,300' rel='stylesheet' type='text/css'>
+    </head>
+    <body data-spy="scroll" data-target=".navbar">
+        <div class="animsition-overlay">
+            <div class="wrapper">
+
+            </div>
+        </div>
+
+        @section('scripts')
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/jquery.animsition.min.js"></script>
+        <script src="cubeportfolio/js/jquery.cubeportfolio.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/lightbox.js"></script>
+        <script src="js/blockets.js"></script>
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <script src="js/ie10-viewport-bug-workaround.js"></script>
+        @stop
+    </body>
+</html>
